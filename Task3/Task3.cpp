de5562c3ae6992aa54b9a185ad6ad60675e2f06d
Harry Hedman <harhed-9@student.ltu.se>
2019-11-21 14:49:04 +0100

    Update: task 3 and 4 done

--- Task3/Task3.cpp
+++ Task3/Task3.cpp
@@ -1,9 +1,9 @@
-#include "iostream"
-#include "cstdlib"
+#include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
-int array_rows_cols(int **a);
+int array_rows_cols(int *a, int c, int r);
 
 int main() {
 	int rows;
@@ -12,16 +12,38 @@ int main() {
 	cin >> rows;
 	cout << "How many columns do you require?" << endl;
 	cin >> columns;
-	int *a = new int[columns * rows];
-	for (int i = 0; i < columns; i++) {
-		
+	int *arr = new int[columns * rows];
+	for (int i = 0; i < columns*rows; i++) {
+		arr[i] = rand() % 10;
 	}
 
+	array_rows_cols(arr, columns, rows);
 
-	system("pause");
+	int x;
+	cin >> x;
 	return 0;
 }
 
-int array_rows_cols(int **a) {
-
+int array_rows_cols(int *a, int c, int r) {
+	int row_sum = 0, column_sum = 0, total_sum = 0;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cout << a[j + i * c] << "\t";
+			row_sum = row_sum + a[j + i * c];
+		}
+		cout << row_sum << endl;
+		total_sum = total_sum + row_sum;
+		row_sum = 0;
+		column_sum = 0;
+	}
+	for (int i = 0; i < c; i++) {
+		for (int k = 0; k < r; k++) {
+			column_sum = column_sum + a[k*c + i];
+		}
+		cout << column_sum << "\t";
+		total_sum = total_sum + column_sum;
+		column_sum = 0;
+	}
+	cout << total_sum << endl;
+	return true;
 }
---
+++ Task5/Task5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void shrink_array(int a[]);
+
+int main() {
+
+
+
+	system("pause");
+	return 0;
+}
+
+void shrink_array(int a[]) {
+
+}
