94092ccebe7430663cc6c7de06a2ca396b601909
Harry Hedman <harhed-9@student.ltu.se>
2019-11-21 17:29:18 +0100

    Task4?

---
+++ Task4/Task4.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+void swap_sort(int &a, int &b, int &c, bool order);
+
+int main() {
+	while (true){
+		int a, b, c;
+		bool order;
+
+		cout << "Give a(0 to quit) :" << endl;
+		cin >> a;
+		if (a == 0) {
+			break;
+		}
+		cout << "Give b :" << endl;
+		cin >> b;
+		cout << "Give c :" << endl;
+		cin >> c;
+		cout << "Sort ascending / descending(1 / 0) :" << endl;
+		cin >> order;
+
+		swap_sort(a, b, c, order);
+
+		cout << "Result : " << a << " " << b << " " << c << endl;
+	}
+
+	return 0;
+}
+
+void swap_sort(int &a, int &b, int &c, bool order) {
+
+	if (order) {
+		int k;
+		if (a > b) {
+			k = b;
+			b = a;
+			a = k;
+		}
+		if (b > c) {
+			k = c;
+			c = b;
+			b = k;
+		}
+		if (a > b) {
+			k = b;
+			b = a;
+			a = k;
+		}
+	}
+	else {
+		int k;
+		if (a < b) {
+			k = b;
+			b = a;
+			a = k;
+		}
+		if (b < c) {
+			k = c;
+			c = b;
+			b = k;
+		}
+		if (a < b) {
+			k = b;
+			b = a;
+			a = k;
+		}
+	}
+
+}
---
+++ lab2lib/lab2lib.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void task6() {
+	int userinput;
+	vector<string> database;
+
+	while (true) {
+		cout << endl << "Menu:" << endl;
+		cout << "1: initialise database" << endl;
+		cout << "2: insert" << endl;
+		cout << "3: search" << endl;
+		cout << "4: delete" << endl;
+		cout << "5: print" << endl;
+		cout << "6: quit" << endl << endl;
+
+		cout << "Make your choice: ";
+		cin >> userinput;
+		if (userinput == 1) {
+			bool sure;
+			cout << "Are you sure? (1/0)" << endl;
+			cin >> sure;
+			if (sure) {
+				database.clear();
+				cout << endl << "Database Initialised" << endl;
+			}
+		}
+		if (userinput == 2) {
+			while (true) {
+				string temp;
+				cout << endl << "Give me a name (Q or q to go back)" << endl;
+				cin >> temp;
+				if (temp == "q" || temp == "Q") {
+					break;
+				}
+				database.push_back(temp);
+			}
+		}
+		if (userinput == 3) {
+			cout << endl << "Give search string" << endl;
+			string temp;
+			cin >> temp;
+			for (int i = 0; i < database.size(); i++) {
+				if (database[i].substr(0, temp.length()) == temp) {
+					cout << endl << database[i] << endl;
+				}
+			}
+
+		}
+		if (userinput == 4) {
+			cout << endl << "Delete which name?" << endl;
+			string temp;
+			cin >> temp;
+			for (int i = 0; i < database.size(); i++) {
+				if (database[i] == temp) {
+					cout << endl << database[i] << " deleted" << endl;
+					database.erase(database.begin() + i);
+					break;
+				}
+			}
+		}
+		if (userinput == 5) {
+			cout << endl << "Contents:" << endl;
+			for (int i = 0; i < database.size(); i++) {
+				cout << database[i] << endl;
+			}
+		}
+		if (userinput == 6) {
+			break;
+		}
+	}
+
+}
